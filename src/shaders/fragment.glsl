def58b90b30ea73e472ac3d8e291d5a75f97a48c
Forpee <khalilhassam3@gmail.com>
2022-05-28 17:41:33 +0200

    update

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -2,7 +2,16 @@ uniform float uTime;
 
 varying vec2 vUv;
 
+float aastep(float threshold,float value){
+    #ifdef GL_OES_standard_derivatives
+    float afwidth=length(vec2(dFdx(value),dFdy(value)))*.70710678118654757;
+    return smoothstep(threshold-afwidth,threshold+afwidth,value);
+    #else
+    return step(threshold,value);
+    #endif
+}
+
 void main()
 {
-    gl_FragColor = vec4(vUv, 1.0, 1.0);
+    gl_FragColor=vec4(vUv,1.,1.);
 }
