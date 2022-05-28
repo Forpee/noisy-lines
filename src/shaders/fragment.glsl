0f13ed7c3029c009b018f94869d017b39e3d8473
Forpee <khalilhassam3@gmail.com>
2022-05-28 17:47:28 +0200

    update

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -18,6 +18,13 @@ float line(vec2 uv){
     return u;
 }
 
+vec2 rotate(vec2 v,float a){
+    float s=sin(a);
+    float c=cos(a);
+    mat2 m=mat2(c,-s,s,c);
+    return m*v;
+}
+
 void main()
 {
     vec2 newUv=gl_FragCoord.xy/uResolution.xy;
