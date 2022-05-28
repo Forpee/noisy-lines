c3efcff49aeaad2c3e6994ae1b0a3b7a24c455db
Forpee <khalilhassam3@gmail.com>
2022-05-28 18:30:26 +0200

    update

--- src/script.js
+++ src/script.js
@@ -5,6 +5,8 @@ import * as dat from 'dat.gui';
 import vertexShader from './shaders/vertex.glsl';
 import fragmentShader from './shaders/fragment.glsl';
 import gsap from 'gsap';
+// import FontLoader from 'examples/jsm/loader/FontLoader.js';
+import { FontLoader } from 'three/examples/jsm/loaders/FontLoader';
 /**
  * Base
  */
@@ -118,6 +120,23 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
  */
 const clock = new THREE.Clock();
 
+const loader = new FontLoader();
+
+loader.load('/font.json', function (font) {
+
+    const geometry = new TextGeometry('Hello three.js!', {
+        font: font,
+        size: 80,
+        height: 5,
+        curveSegments: 12,
+        bevelEnabled: true,
+        bevelThickness: 10,
+        bevelSize: 8,
+        bevelOffset: 0,
+        bevelSegments: 5
+    });
+});
+
 const tick = () => {
     // Update controls
     controls.update();
@@ -127,7 +146,10 @@ const tick = () => {
 
     // Update uniforms
     material.uniforms.uTime.value = elapsedTime;
-
+    mouseTarget.x -= 0.1 * (mouseTarget.x - mouse.x);
+    mouseTarget.y -= 0.1 * (mouseTarget.y - mouse.y);
+    boxMesh.rotation.y = mouseTarget.x;
+    boxMesh.rotation.x = mouseTarget.y;
     // Render
     renderer.render(scene, camera);
 
